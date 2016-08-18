e95a22b710142d6ff989dda3e9d5c949b8ab7773
Bryan <btteh@hotmail.com>
2016-08-18 15:47:47 +0800

    Finished adding tile collision in Hero.cpp

--- SP3 Framework/Base/Source/Hero.cpp
+++ SP3 Framework/Base/Source/Hero.cpp
@@ -27,26 +27,29 @@ void Hero::Update(const double &deltaTime)
 		sizeDiff.x = tileSystem->GetTileSize() - scale.x;
 		sizeDiff.y = tileSystem->GetTileSize() - scale.y;
 
-		switch (tile->GetTileValue(Tile::TILE_TYPE::TERRAIN)) 
+		if (hotspotPosition.x < tileSystem->GetLeftBorder())
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::TERRAIN))
 		{
 		case TILE_WALL_1:
 		{
-			position += speed - sizeDiff.x;
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
 			break;
 		}
 		case TILE_WALL_2:
 		{
-			position += speed - sizeDiff.x;
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
 			break;
 		}
 		case TILE_WALL_3:
 		{
-			position += speed - sizeDiff.x;
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
 			break;
 		}
 		}
 
-		switch (tile->GetTileValue(Tile::TILE_TYPE::ITEM)) 
+		switch (tile->GetTileValue(Tile::TILE_TYPE::ITEM))
 		{
 		case TILE_COIN:
 		{
@@ -61,8 +64,6 @@ void Hero::Update(const double &deltaTime)
 			break;
 		}
 		}
-
-
 	}
 	// Right
 	for (int i = 0; i < tCollision.GetNumHotspotsHeight(); i++)
@@ -72,8 +73,54 @@ void Hero::Update(const double &deltaTime)
 		hotspotPosition.x = position.x + tCollision.GetDetectionWidth() * 0.5f;
 		hotspotPosition.y = minY + i * tCollision.GetHotspotOffsetHeight();
 
-		// switch (TileStuff)
+		int tileX = tileSystem->GetTile(hotspotPosition.x);
+		int tileY = tileSystem->GetTile(hotspotPosition.y);
+
+		Tile* tile = &tileSystem->tiles[tileY][tileX];
+
+		Vector2 sizeDiff;
+		sizeDiff.x = tileSystem->GetTileSize() - scale.x;
+		sizeDiff.y = tileSystem->GetTileSize() - scale.y;
+
+		if (hotspotPosition.x > tileSystem->GetRightBorder())
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::TERRAIN))
+		{
+		case TILE_WALL_1:
+		{
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
+			break;
+		}
+		case TILE_WALL_2:
+		{
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
+			break;
+		}
+		case TILE_WALL_3:
+		{
+			position.x = tileX * tileSystem->GetTileSize() + sizeDiff.x * 0.5f;
+			break;
+		}
+		}
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::ITEM))
+		{
+		case TILE_COIN:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			score++;
+			break;
+		}
+		case TILE_CHECKPOINT_UNSET:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			tile->tileValue |= TILE_CHECKPOINT_SET;
+			break;
+		}
+		}
 	}
+
 	// Up
 	for (int i = 0; i < tCollision.GetNumHotspotsWidth(); i++)
 	{
@@ -82,7 +129,52 @@ void Hero::Update(const double &deltaTime)
 		hotspotPosition.x = minX + i * tCollision.GetHotspotOffsetWidth();
 		hotspotPosition.y = position.y + tCollision.GetDetectionHeight() * 0.5f;
 
-		// switch (TileStuff)
+		int tileX = tileSystem->GetTile(hotspotPosition.x);
+		int tileY = tileSystem->GetTile(hotspotPosition.y);
+
+		Tile* tile = &tileSystem->tiles[tileY][tileX];
+
+		Vector2 sizeDiff;
+		sizeDiff.x = tileSystem->GetTileSize() - scale.x;
+		sizeDiff.y = tileSystem->GetTileSize() - scale.y;
+
+		if (hotspotPosition.y > tileSystem->GetTopBorder())
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::TERRAIN))
+		{
+		case TILE_WALL_1:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		case TILE_WALL_2:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		case TILE_WALL_3:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		}
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::ITEM))
+		{
+		case TILE_COIN:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			score++;
+			break;
+		}
+		case TILE_CHECKPOINT_UNSET:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			tile->tileValue |= TILE_CHECKPOINT_SET;
+			break;
+		}
+		}
 	}
 	// Down
 	for (int i = 0; i < tCollision.GetNumHotspotsHeight(); i++)
@@ -92,7 +184,52 @@ void Hero::Update(const double &deltaTime)
 		hotspotPosition.x = minX + i * tCollision.GetHotspotOffsetWidth();
 		hotspotPosition.y = position.y - tCollision.GetDetectionHeight() * 0.5f;
 
-		// switch (TileStuff)
+		int tileX = tileSystem->GetTile(hotspotPosition.x);
+		int tileY = tileSystem->GetTile(hotspotPosition.y);
+
+		Tile* tile = &tileSystem->tiles[tileY][tileX];
+
+		Vector2 sizeDiff;
+		sizeDiff.x = tileSystem->GetTileSize() - scale.x;
+		sizeDiff.y = tileSystem->GetTileSize() - scale.y;
+
+		if (hotspotPosition.y < tileSystem->GetBottomBorder())
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::TERRAIN))
+		{
+		case TILE_WALL_1:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		case TILE_WALL_2:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		case TILE_WALL_3:
+		{
+			position.y = tileY * tileSystem->GetTileSize() + sizeDiff.y * 0.5f;
+			break;
+		}
+		}
+
+		switch (tile->GetTileValue(Tile::TILE_TYPE::ITEM))
+		{
+		case TILE_COIN:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			score++;
+			break;
+		}
+		case TILE_CHECKPOINT_UNSET:
+		{
+			tile->ClearTileValue(Tile::TILE_TYPE::ITEM);
+			tile->tileValue |= TILE_CHECKPOINT_SET;
+			break;
+		}
+		}
 	}
 }
 
